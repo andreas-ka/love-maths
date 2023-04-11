3e86b3146a1a000f46c0bd1609e79b0a8240684a
andreas-ka <>
2023-04-11 12:36:28 +0000

    add code to increment score and incorrect answers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,8 +45,10 @@ function checkAnswer() {
     let isCorrect = userAnswer === calculatedAnswer[0];
         if (isCorrect) {
             alert("Congrats you guess correct!");
+            incrementScore();
         } else {
             alert(`Awww... you guessed wrong ${userAnswer} the right answer is ${calculatedAnswer[0]}`);
+            incrementWrongAnswer();
         }
 
         runGame(calculatedAnswer[1]);
@@ -68,12 +70,18 @@ function calculateCorrectAnswer() {
     }
 }
 
+/** Gets the current score from the Dom and increments by 1 */
 function incrementScore() {
 
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 }
 
+/** Gets the incorrect answer from the Dom and increments by 1 */
 function incrementWrongAnswer() {
 
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
